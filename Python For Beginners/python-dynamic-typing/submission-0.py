39a6c38a6f242884165ed38dafc94469b2c87bfa
Hamida Mohamed <hamidamohamed940@gmail.com>
2026-09-18 15:34:12 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 25
+print(type(variable))
+variable = 092.3
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "nawww"
+print(type(variable))
+variable = [2,34,3]
+print(type(variable))
