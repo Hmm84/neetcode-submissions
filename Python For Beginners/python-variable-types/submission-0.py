438140bedd600ac2a09ebbdd58ea0ab306802f61
Hamida Mohamed <hamidamohamed940@gmail.com>
2026-09-18 15:32:34 -0500

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 24
+float_type = 99.2
+bool_type = False
+string_type = "Something red"
+list_type = [2, 34, 4]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
